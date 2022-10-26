d3f2e2611188aaf33ccccd167217534053974ad9
aungmk <aunglay265@gmail.com>
2022-10-26 22:43:49 +0630

    testing react prog

---
+++ android/app/src/main/kotlin/com/example/padc_reactive_programming/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.padc_reactive_programming
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+Future<void> main() async{
+  Future<String> nameFuture =Future.value("PADC");
+
+  nameFuture.then((name) => print(name));
+
+  Future<List<int>> numberListFuture=
+      Future.value([1,2,3,4,5,6,7]);
+  numberListFuture.then((numberList) => print(numberList.toString()));
+
+  Future<String> errorFuture= Future.error(NullThrownError());
+
+  errorFuture.then((value) {
+    print(value);
+  }).catchError((error){
+    print(error);
+  });
+
+  Future<String> delayedNameFuture=
+      Future.delayed(Duration(milliseconds: 3000),()=> "AMK");
+  delayedNameFuture.then((value) => print(value));
+}
